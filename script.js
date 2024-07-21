62bdd65e5a2c2b3afb4042f71d1c32a92b2b1188
Mariana8819 <mivallejos8819@gmail.com>
2024-07-21 13:49:19 -0300

    portafolio ok al 90%

--- script.js
+++ script.js
@@ -39,3 +39,25 @@ function efectoHabilidades(){
         document.getElementById("ps").classList.add("barra-progreso4");
     }
 }
+
+//Inicia EmailJS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_eljxbcq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
